b13978a966b37e238886d134b7b5959324a24e77
numisteinn <43776582+numisteinn@users.noreply.github.com>
2026-09-10 13:49:07 +0000

    Update footer date automatically

--- footer.js
+++ footer.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const footer = document.querySelector("footer");
   if (footer) {
-    footer.textContent = "Last updated: March 2026";
+    const last_updated = new Date(document.lastModified);
+    const formatted_date = new Intl.DateTimeFormat("en", {
+      month: "long",
+      year: "numeric",
+    }).format(last_updated);
+
+    footer.textContent = `Last updated: ${formatted_date}`;
   }
 });
